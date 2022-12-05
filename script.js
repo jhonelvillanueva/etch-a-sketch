6ed7dd91c439fc7691e63720188febb8993b5c26
Jhonel Villanueva <vvjhonel@gmail.com>
2022-12-06 01:12:22 +0800

    Add the grid board

---
+++ script.js
@@ -0,0 +1,8 @@
+const boardContainer = document.querySelector('.board-container');
+
+for (i = 0; i < 256; i++) {
+  const boardDiv = document.createElement('div');
+  boardDiv.classList.add('single-div');
+  boardContainer.appendChild(boardDiv);
+  console.log(i);
+}
